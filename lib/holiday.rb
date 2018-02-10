40692816a2dba1bd8050b122914396674180f355
Chris Henry <cdhenry@gmail.com>
2018-02-10 00:11:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season}".capitalize + ":"
     holidays.each do |day, supplies|
+      if day.include? "_"
+        day.sub!("_", " ")
       puts "  " + "#{day}".capitalize + ": #{supplies.join(", ")}"
     end
   end
