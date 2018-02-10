de5dcabc93b05be1d749b04051862d068e99bdaf
Chris Henry <cdhenry@gmail.com>
2018-02-10 00:23:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday = day.to_s
       if holiday.include? "_"
         array = holiday.split('_')
-        array.each {|word| word = "#{word}".capitalize}
+        array.each {|word| word = word.capitalize}
         holiday = array.join(" ")
       end
       puts "  " + "#{holiday}".capitalize + ": #{supplies.join(", ")}"
