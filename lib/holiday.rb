42ab8969376e73aa41a7c912983a523b8f8f4423
Chris Henry <cdhenry@gmail.com>
2018-02-10 00:06:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,15 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season}".capitalize + ":"
     holidays.each do |day, supplies|
-      holiday = "  " + "#{day}".capitalize + ": "
-      puts holiday
-      supplies.each do |item|
-        if item == supplies[-1]
-          puts "#{item}"
-        else
-          print "#{item}, "
-        end
-      end
+      puts "  " + "#{day}".capitalize + ": " + supplies.values
     end
   end
 end
